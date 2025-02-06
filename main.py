91fdc2a893f149b1de602c864c736b840af1da21
Ian To <onlyian4981@gmail.com>
2025-02-06 10:57:51 -0800

    main.py: touch and initialize file
    Add minimal function required by assignment.
    
    Signed-off-by: Ian To <onlyian4981@gmail.com>

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+import string
+import requests
+
+# Generate single random char
+def rand_char():
+    choices = string.ascii_letters + string.digits + string.punctuation
+    
+    return random.choice(choices)
+
+password_length = int(input("Please enter the length you want your password to be:\n"))
+
+# Generate a password of random chars
+def gen_strong_pass():
+    password = ""
+
+    for i in range(password_length):
+        password = password + rand_char()
+    
+    print(password)
+
+gen_strong_pass()
+
+# GET a rand word from dictionary API
+def fetch_word():
+    url = "https://random-word-api.herokuapp.com/word?length=4"
+
+    response = requests.get(url)
+    word = response.json()[0]
+
+    return word
+
+print(fetch_word())
