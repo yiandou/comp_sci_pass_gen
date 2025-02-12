cbf90960927903ddc3999a07dbcde854e5fe8dc5
Ian To <onlyian4981@gmail.com>
2025-02-12 10:24:01 -0800

    main.py: added function to strengthen password
    Strengthen the password generated with dictionary words by replacing
    matched strings with other chars.
    
    Signed-off-by: Ian To <onlyian4981@gmail.com>

--- main.py
+++ main.py
@@ -30,10 +30,28 @@ def fetch_word():
 
     return word
 
+def replace_letters(word: str):
+    word = word[0].upper() + word[1:]
+    
+    if "a" in word:
+        word = word.replace("a", "@")
+    if "e" in word:
+        word = word.replace("e", "3")
+    if "l" in word:
+        word = word.replace("l", "|")
+    if "o" in word:
+        word = word.replace("o", "()")
+    if "s" in word:
+        word = word.replace("s", "$")
+
+    return word
+
 def gen_weaker_pass():
     word1 = fetch_word()
     word2 = fetch_word()
     password = word1 + word2
+    password = replace_letters(password)
+
     return password
 
 print(gen_weaker_pass())
